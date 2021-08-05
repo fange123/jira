11657cf188ad930b0ce0647a29465eaad62af200
fange123 <2030564240@qq.com>
2021-08-05 14:56:37 +0800

    dev:修改了一点

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <ProjectListScreen /> */}
-      <TsReactTest />
+      <ProjectListScreen />
+      {/* <TsReactTest /> */}
     </div>
   );
 }
