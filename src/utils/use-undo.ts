a1ea041b74fdf2e8f36a44a8a637d6fd882e8e0c
fange123 <2030564240@qq.com>
2021-12-01 16:39:49 +0800

    feat:用useReducer进行状态管理

--- src/utils/use-undo.ts
+++ src/utils/use-undo.ts
@@ -1,49 +1,40 @@
-import { useCallback, useState } from "react"
+import { useCallback, useReducer } from "react"
 
-export const useUndo = <T>(initialPresent:T)=> {
-  // const [past, setPast] = useState<T[]>([])
-  // const [present, setPresent]  = useState(initialPresent)
-  // const [feature, setFeature] = useState<T[]>([])
+const UNDO = 'UNDO'
+const REDO = 'REDO'
+const SET = 'SET'
+const RESET = 'RESET'
 
-  //~状态合并
-
-  const [state, setState] = useState<{
-    past: T[],
+type State<T> ={
+  past: T[],
     present: T,
     feature: T[],
-  }>({
-    past:[],
-    present:initialPresent,
-    feature:[]
-  })
 
-  const canUndo = state.past.length!==0
-  const canRedo = state.feature.length!==0
+}
 
-  const undo = useCallback(()=>{
+type Action<T> ={newPresent?:T,type:typeof UNDO|typeof SET|typeof REDO|typeof RESET}
 
-    setState(currentState=> {
-      const {past, present, feature} = currentState
-      if(past.length === 0) return currentState
+const undoReducer = <T>(state:State<T>,action:Action<T>)=> {
+
+  const {past,present,feature} = state
+
+  const {newPresent} = action
+
+  switch (action.type) {
+    case UNDO:{
+      if(past.length === 0) return state
       const previous = past[past.length - 1]
       const newPast =past.slice(0,past.length-1)
 
-    return {
-      past: newPast,
-      present: previous,
-      feature:[present,...feature]
+      return {
+        past: newPast,
+        present: previous,
+        feature:[present,...feature]
+      }
+
     }
-
-
-  })
-},[])
-
-
-  const redo = useCallback(()=> {
-
-    setState(currentState=>{
-      const {past, present, feature} = currentState
-       if(feature.length === 0) return currentState
+    case REDO:{
+       if(feature.length === 0) return state
 
       const next = feature[0]
       const newFeature = feature.slice(1)
@@ -54,14 +45,11 @@ export const useUndo = <T>(initialPresent:T)=> {
         feature:newFeature
       }
 
-    })
 
-  },[])
+    }
 
-  const set = useCallback((newPresent:T) => {
-    setState(currentState=> {
-      const {past, present} = currentState
-      if(newPresent===present) return currentState
+    case SET:{
+      if(newPresent===present) return state
 
       return {
         past: [...past,present],
@@ -69,19 +57,59 @@ export const useUndo = <T>(initialPresent:T)=> {
         feature:[]
       }
 
-    })
+    }
 
-  },[])
-
-  const reset = useCallback((newPresent:T) => {
-    setState(()=> {
+    case RESET:{
       return {
             past: [],
             present: newPresent,
             feature:[]
           }
-      })
-  },[])
+    }
+
+
+    default:
+      break;
+  }
+
+  return state
+
+}
+
+
+export const useUndo = <T>(initialPresent:T)=> {
+  // const [past, setPast] = useState<T[]>([])
+  // const [present, setPresent]  = useState(initialPresent)
+  // const [feature, setFeature] = useState<T[]>([])
+
+  //~状态合并
+
+  // const [state, setState] = useState<{
+  //   past: T[],
+  //   present: T,
+  //   feature: T[],
+  // }>({
+  //   past:[],
+  //   present:initialPresent,
+  //   feature:[]
+  // })
+
+ const [state, dispatch] = useReducer(undoReducer, {
+    past:[],
+    present:initialPresent,
+    feature:[]
+ } as State<T>)
+
+  const canUndo = state.past.length!==0
+  const canRedo = state.feature.length!==0
+
+  const undo = useCallback(()=> dispatch({type:'UNDO'}) ,[])
+
+  const redo = useCallback(()=> dispatch({type:'REDO'}) ,[])
+
+  const set = useCallback((newPresent:T) => dispatch({type:'SET',newPresent}),[])
+
+  const reset = useCallback((newPresent:T) => dispatch({type:"RESET",newPresent}),[])
 
   //!如果在自定义的hook里面return出函数，最好需要useCallback
   return[
