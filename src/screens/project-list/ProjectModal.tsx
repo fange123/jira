b2f0a15c60790d3a04f8232bc2f5030986255293
fange123 <2030564240@qq.com>
2021-12-06 09:55:06 +0800

    faet:创建编辑页面布局

--- src/screens/project-list/ProjectModal.tsx
+++ src/screens/project-list/ProjectModal.tsx
@@ -5,6 +5,7 @@ import UserSelect from "components/user-select";
 import { useAddProject, useEditProject } from "utils/project";
 import { useForm } from "antd/lib/form/Form";
 import { ErrorBox } from "components/lib";
+import styled from "styled-components";
 
 interface IProps {}
 
@@ -26,49 +27,59 @@ const ProjectModal: React.FC<IProps> = (props) => {
   }, [editProject, form]);
   return (
     <Drawer visible={projectModalOpen} width="100%" onClose={close}>
-      {isLoading ? (
-        <Spin size="large" />
-      ) : (
-        <>
-          <h1>{title}</h1>
-          <ErrorBox error={error} />
-          <Form
-            form={form}
-            layout="vertical"
-            style={{ width: "40rem" }}
-            onFinish={onFinish}
-          >
-            <Form.Item
-              label="项目名称"
-              name="name"
-              rules={[{ required: true, message: "请输入项目名称" }]}
+      <Container>
+        {isLoading ? (
+          <Spin size="large" />
+        ) : (
+          <>
+            <h1>{title}</h1>
+            <ErrorBox error={error} />
+            <Form
+              form={form}
+              layout="vertical"
+              style={{ width: "40rem" }}
+              onFinish={onFinish}
             >
-              <Input placeholder="请输入项目名称" />
-            </Form.Item>
-            <Form.Item
-              label="部门"
-              name="organization"
-              rules={[{ required: true, message: "请输入部门名称" }]}
-            >
-              <Input placeholder="请输入部门名称" />
-            </Form.Item>
-            <Form.Item
-              label="负责人"
-              name="personId"
-              rules={[{ required: true, message: "请输入负责人名称" }]}
-            >
-              <UserSelect defaultOptionName="负责人" />
-            </Form.Item>
-            <Form.Item>
-              <Button loading={mutateLoading} type="primary" htmlType="submit">
-                提交
-              </Button>
-            </Form.Item>
-          </Form>
-        </>
-      )}
+              <Form.Item
+                label="项目名称"
+                name="name"
+                rules={[{ required: true, message: "请输入项目名称" }]}
+              >
+                <Input placeholder="请输入项目名称" />
+              </Form.Item>
+              <Form.Item
+                label="部门"
+                name="organization"
+                rules={[{ required: true, message: "请输入部门名称" }]}
+              >
+                <Input placeholder="请输入部门名称" />
+              </Form.Item>
+              <Form.Item label="负责人" name="personId">
+                <UserSelect defaultOptionName="负责人" />
+              </Form.Item>
+              <Form.Item style={{ textAlign: "right" }}>
+                <Button
+                  loading={mutateLoading}
+                  type="primary"
+                  htmlType="submit"
+                >
+                  提交
+                </Button>
+              </Form.Item>
+            </Form>
+          </>
+        )}
+      </Container>
     </Drawer>
   );
 };
 
 export default ProjectModal;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+  flex-direction: column;
+`;
--- src/un-auth/index.tsx
+++ src/un-auth/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Divider, Typography } from "antd";
+import { Button, Card, Divider } from "antd";
 import React, { useState } from "react";
 import Login from "./login";
 import Register from "./register";
