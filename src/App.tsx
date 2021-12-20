1dd7a73ebc65b5e17fb14424d064943801834c2c
张海玉 <2030564240@qq.com>
2021-12-20 11:20:32 +0800

    feat:代码分割优化

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import Auth from "./Auth";
-import UnAuth from "./un-auth";
 import { useAuth } from "context/auth-context";
-import { FullPageError } from "components/lib";
+import { FullPageError, FullPageLoading } from "components/lib";
 import { ErrorBoundary } from "./components/error-boundary";
+import React from "react";
 
 function App() {
   const { user } = useAuth();
+  //TODO:代码分割优化，lazy里面是promise函数,配合Suspense
+  const AuthApp = React.lazy(() => import("Auth"));
+  const UnAuthApp = React.lazy(() => import("un-auth"));
 
   return (
     <div className="App">
       <ErrorBoundary fallBack={FullPageError}>
-        {user ? <Auth /> : <UnAuth />}
+        <React.Suspense fallback={<FullPageLoading />}>
+          {user ? <AuthApp /> : <UnAuthApp />}
+        </React.Suspense>
       </ErrorBoundary>
     </div>
   );
